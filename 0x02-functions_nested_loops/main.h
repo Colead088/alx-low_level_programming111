e79ed9254618629e64551e56c69423ef7cdc678f
Colead088 <christopher.olasupo@gmail.com>
2022-09-22 16:28:55 -0700

    yer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
  #include <unistd.h>
 
  void print_alphabet(void);
+ void print_alphabet_x10(void);
  int _putchar(char c);
  void positive_or_negative(int i);
  int largest_number(int a, int b, int c);
