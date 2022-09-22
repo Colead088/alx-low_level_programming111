40169acf4d0040346579c8949a1c8f08cb162689
Colead088 <christopher.olasupo@gmail.com>
2022-09-22 15:45:50 -0700

    Get

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,14 @@
- #ifndef main.h
- #define main.h
+ #ifndef main_h
+ #define main_h
 
  #include <stdio.h>
 
+ int _putchar(char c);
  void positive_or_negative(int i);
  int largest_number(int a, int b, int c);
  int convert_day(int month, int day);
  void print_remaining_days(int month, int day, int year);
 
- #endif /* main.h */
+ #endif /* main_h */
+ 
 
