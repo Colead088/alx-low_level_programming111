576a23a7c85bbf78af249d116e9540b0b2426c1d
Colead088 <christopher.olasupo@gmail.com>
2022-09-24 14:19:56 -0700

    Going well

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int largest_number(int, int, int);
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 int convert_day(int month, int day);                                                                                                                     
 void print_remaining_days(int month, int day, int year);                                                                                                 
 #endif /* MAIN_H */
