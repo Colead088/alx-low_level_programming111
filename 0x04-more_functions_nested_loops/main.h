0ffa9b30429b6dc1d75fffc1310547466d28305e
Colead088 <christopher.olasupo@gmail.com>
2022-09-24 04:34:41 -0700

    Go again

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 int largest_number(int, int, int);                                                                                                                       
 int convert_day(int month, int day);                                                                                                                     
 void print_remaining_days(int month, int day, int year);                                                                                                 
