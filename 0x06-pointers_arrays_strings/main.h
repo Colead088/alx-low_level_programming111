26c6b34afeaf132e5ff33ca921ecb93ec1d3119e
Colead088 <christopher.olasupo@gmail.com>
2022-09-25 16:26:33 -0700

    Yeah

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H                                                                                                                                           
+#define MAIN_H                                                                                                                                           
+                                                                                                                                                         
+#include <stdio.h>                                                                                                                                       
+                                                                                                                                                         
+void positive_or_negative(int i);                                                                                                                        
+int largest_number(int, int, int);                                                                                                                       
+void reset_to_98(int *n);                                                                                                                                
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+void swap_int(int *a, int *b);                                                                                                                           
+int _strlen(char *s);                                                                                                                                    
+void _puts(char *str);                                                                                                                                   
+int _putchar(char c);                                                                                                                                    
+void reset_to_98(int *n);                                                                                                                                
+void swap_int(int *a, int *b);                                                                                                                           
+int _strlen(char *s);                                                                                                                                    
+void _puts(char *str);                                                                                                                                   
+void print_rev(char *s);                                                                                                                                 
+void rev_string(char *s);                                                                                                                                
+void puts2(char *str);                                                                                                                                   
+void puts_half(char *str);                                                                                                                               
+void print_array(int *a, int n);                                                                                                                         
+char *_strcpy(char *dest, char *src);                                                                                                                    
+int _atoi(char *s);                                                                                                                                      
+int convert_day(int month, int day);                                                                                                                     
+void print_remaining_days(int month, int day, int year);                                                                                                 
+#endif /* MAIN_H */
