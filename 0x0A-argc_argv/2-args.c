878b398191d87912ae065a2c7dc3d4f736d7f713
Colead088 <christopher.olasupo@gmail.com>
2022-10-05 10:00:03 -0700

    Task All

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 /**
- *  * main - A program that prints all arguements it receives
- *   * @argc: The arguement counter
- *    * @argv: The arguement values
- *     * Return: Always 0 (Success)
- *      */
+ * main - A program that prints all arguements it receives
+ * @argc: The arguement counter
+ * @argv: The arguement values
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char **argv)
 {
-		while (argc--)
-				{
-							printf("%s\n", *argv++);
-								}
-			return (0);
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
 }
