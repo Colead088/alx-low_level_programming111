d4c92910f4b882563744e9caaafcf0a0e63554ec
Colead088 <christopher.olasupo@gmail.com>
2022-10-05 07:32:56 -0700

    Task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,7 @@
-fndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H                                                                                                                                           
+#define MAIN_H                                                                                                                                           
+                                                                                                                                                         
+#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,6 +23,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* MAIN_H_ */
+#endif /* MAIN_H */
 
