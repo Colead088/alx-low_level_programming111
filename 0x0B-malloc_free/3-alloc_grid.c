479ed3074fa5891ee85297c4306562e8c668acd5
Colead088 <christopher.olasupo@gmail.com>
2022-10-06 04:39:27 -0700

    Go

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
 
 /**
  * alloc_grid - A function that returns a pointer to a
